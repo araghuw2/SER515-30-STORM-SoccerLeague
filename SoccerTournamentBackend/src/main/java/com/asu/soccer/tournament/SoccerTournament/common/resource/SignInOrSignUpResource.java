d0ec4c8790527b90a6cbdaa7b5985b016e36a438
Namrata Gaur <ngaur5@asu.edu>
2021-10-12 14:12:37 -0700

    US-47-Task-67: Added content-types to resources

--- SoccerTournamentBackend/src/main/java/com/asu/soccer/tournament/SoccerTournament/common/resource/SignInOrSignUpResource.java
+++ SoccerTournamentBackend/src/main/java/com/asu/soccer/tournament/SoccerTournament/common/resource/SignInOrSignUpResource.java
@@ -1,12 +1,15 @@
 package com.asu.soccer.tournament.SoccerTournament.common.resource;
 
+import java.util.HashMap;
 import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 
 import com.asu.soccer.tournament.SoccerTournament.common.entity.UserEntity;
 
 interface SignInOrSignUpResource {
 	
 	public List<String> signUp(UserEntity user);
-	public UserEntity signIn(String username, String password);
+	public ResponseEntity<UserEntity> signIn(HashMap<String, String> hm);
 	
 }
--- SoccerTournamentBackend/src/main/java/com/asu/soccer/tournament/SoccerTournament/common/resource/SignInOrSignUpResourceImpl.java
+++ SoccerTournamentBackend/src/main/java/com/asu/soccer/tournament/SoccerTournament/common/resource/SignInOrSignUpResourceImpl.java
@@ -1,16 +1,19 @@
 package com.asu.soccer.tournament.SoccerTournament.common.resource;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.asu.soccer.tournament.SoccerTournament.common.entity.PlayerEntity;
 import com.asu.soccer.tournament.SoccerTournament.common.entity.UserEntity;
 import com.asu.soccer.tournament.SoccerTournament.common.service.SignInOrSignUpService;
-import com.asu.soccer.tournament.SoccerTournament.player.repository.PlayerRepository;
 
 
 @RestController
@@ -29,9 +32,13 @@ class SignInOrSignUpResourceImpl implements SignInOrSignUpResource {
 	}
 	
 	@Override
-	@PostMapping("/signin")
-	public UserEntity signIn(String username, String password) {
-		return signInOrSignUpService.signIn(username, password);
+	@PostMapping(path = "/signin",consumes = MediaType.APPLICATION_JSON_VALUE,  produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UserEntity> signIn(@RequestBody HashMap<String, String> hm) {
+		UserEntity user = signInOrSignUpService.signIn(hm.get("username"), hm.get("password"));
+		if (user != null) {
+            return new ResponseEntity<UserEntity>(user, HttpStatus.OK);
+        }
+        return new ResponseEntity<UserEntity>(HttpStatus.NOT_FOUND);
 	}
 
 }
