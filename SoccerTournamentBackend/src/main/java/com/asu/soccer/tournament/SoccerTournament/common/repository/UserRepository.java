be1e146f0a707ec28b675fc7f8e9ea542f448e0d
TapaswiBusireddy <tbusired@asu.edu>
2021-10-28 16:18:58 -0700

    US-75-Task-79: Incorporated review comments

--- SoccerTournamentBackend/src/main/java/com/asu/soccer/tournament/SoccerTournament/common/repository/UserRepository.java
+++ SoccerTournamentBackend/src/main/java/com/asu/soccer/tournament/SoccerTournament/common/repository/UserRepository.java
@@ -15,17 +15,9 @@ import com.asu.soccer.tournament.SoccerTournament.common.entity.UserEntity;
 @Repository
 @EnableJpaRepositories
 public interface UserRepository extends PagingAndSortingRepository<UserEntity,Long>{
-//	UserEntity findById(String id);
 	UserEntity save(UserEntity user);
 
 	@PersistenceContext
-//	EntityManager entityManager;
-//	this.entityManager.persist(user_details);
-//	    entityManager.createNativeQuery("INSERT INTO user_details (id, first_name, last_name) VALUES (?,?,?)")
-//	      .setParameter(1, user_details.getId())
-//	      .setParameter(2, user_details.getFirstName())
-//	      .setParameter(3, user_details.getLastName())
-//	      .executeUpdate();
 	
     @Query(value = "SELECT * FROM user_details u WHERE u.email = ?1 and u.password = ?2", nativeQuery = true)
     UserEntity getUser(String username, String password);
