9b150848ddb930b5ecfcab4798750673ba38d585
Vidhya Sathyalaya Ramakrishnan <vsathyal@asu.edu>
2021-11-27 00:35:29 -0700

    US_10_Task_165 Remove age from response json

--- SoccerTournamentBackend/src/main/java/com/asu/soccer/tournament/SoccerTournament/common/model/VolunteerModel.java
+++ SoccerTournamentBackend/src/main/java/com/asu/soccer/tournament/SoccerTournament/common/model/VolunteerModel.java
@@ -4,7 +4,6 @@ public class VolunteerModel {
 
     private String volunteerName;
 	
-	private Integer age;
 	
 	private String email;
 	
@@ -18,13 +17,7 @@ public class VolunteerModel {
 		this.volunteerName = volunteerName;
 	}
 
-	public Integer getAge() {
-		return age;
-	}
 
-	public void setAge(Integer age) {
-		this.age = age;
-	}
 
 	public String getEmail() {
 		return email;
