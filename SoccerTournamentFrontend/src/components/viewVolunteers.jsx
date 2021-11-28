6c71d0474246439c5058676f4406529dcf01212a
TapaswiBusireddy <tbusired@asu.edu>
2021-11-28 15:58:50 -0700

    Successfully integrated for view volunteers

--- SoccerTournamentFrontend/src/components/viewVolunteers.jsx
+++ SoccerTournamentFrontend/src/components/viewVolunteers.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import history from '../history';
+import { viewVolunteers } from '../actions';
 import { connect } from 'react-redux';
 
 class ViewVolunteers extends React.Component {
   
+    componentWillMount() {
+        this.props.viewVolunteers();
+        console.log(this.props.viewVolunteers());
+      }
   render() {
     return (
         <div>
@@ -16,11 +22,31 @@ class ViewVolunteers extends React.Component {
                 <th>Email</th>
                 <th>Gender</th>
             </tr>
+            { this.props.volunteers.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item.firstName}</td>
+                  <td>{item.lastName}</td>
+                  <td>{item.email}</td>
+                  <td>{item.gender}</td>
+                </tr>
+              );
+            })}
         </table>
         <br/>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+    return { 
+        volunteers: state.auth.volunteers,
+        email: state.auth.email,
+        gender: state.auth.gender,
+        firstName: state.auth.firstName,
+        lastName: state.auth.lastName,
+    }
+  }
   
-export default ViewVolunteers;
+  export default connect(mapStateToProps, {viewVolunteers})(ViewVolunteers);
